124bfcf0957838d5e0cea183edcb8f4b49b9c598
Daniel Zvara <dan.zvara@gmail.com>
2017-10-17 15:31:04 +0100

    solidity: example seller contract WIP
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

---
+++ src/solidity/seller.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.11;
+
+contract seller {
+
+  address owner;
+  int energy;
+
+  function seller() public {}
+
+  function buy(int amount) returns (int x) {
+    if (msg.value > 1 ether) {
+      owner.transfer(1 ether);
+      return 1;
+    }
+
+    return -1;
+  }
+
+  function die() public {
+    if (msg.sender == owner) {
+      selfdestruct(owner);
+    }
+  }
+}
