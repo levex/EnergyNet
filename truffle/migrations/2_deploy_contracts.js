6197c618955d062a0434195e187e3a14ecb89748
Daniel Zvara <dan.zvara@gmail.com>
2017-10-27 16:18:29 +0100

    migrations: deploy energy contract
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ConvertLib = artifacts.require("./ConvertLib.sol");
+const MetaCoin = artifacts.require("./MetaCoin.sol");
 const EnergyMaster = artifacts.require('./EnergyMaster.sol');
+const Energy = artifacts.require('./Energy.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(EnergyMaster);
+  deployer.deploy(Energy, EnergyMaster.address, 0x42, 2);
 };
