13cb4ab32e2fd1992b5b0063db0432550c224d6b
Daniel Zvara <dan.zvara@gmail.com>
2017-10-28 19:36:00 +0100

    test: fixed truffle shit && added test for consume
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -3,11 +3,14 @@ const MetaCoin = artifacts.require("./MetaCoin.sol");
 const EnergyMaster = artifacts.require('./EnergyMaster.sol');
 const Energy = artifacts.require('./Energy.sol');
 
-module.exports = function(deployer) {
+var accounts;
+web3.eth.getAccounts(function(err,res) { accounts = res; });
+
+module.exports = (deployer) => {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(EnergyMaster).then(() => {
-    deployer.deploy(Energy, EnergyMaster.address, 0x42, 2);
+    return deployer.deploy(Energy, EnergyMaster.address, accounts[1], 2);
   });
 };
--- truffle/test/TestEnergy.js
+++ truffle/test/TestEnergy.js
@@ -2,34 +2,51 @@ const Energy = artifacts.require('./Energy.sol');
 
 contract('TestEnergy', function(accounts) {
     const seller = accounts[0]
-    const buyer = 0x42
+    const buyer = accounts[1]
 
     it('getBalance should be 0 after contract is created', () => {
-        Energy.deployed().then(instance => {
-            instance.getBalance.call().then(balance => {
-              assert.equal(balance, 0, 'balance is not 0')
-            })
+        return Energy.deployed().then(instance => {
+          return instance.getBalance.call().then(balance => {
+            assert.equal(balance.valueOf(), 0, 'balance is not 0')
+          })
         })
     })
 
     it('getPrice should equal unit price', () => {
-      Energy.deployed().then(instance => {
-        instance.getPrice.call().then(price => {
-          assert.equal(price, 2, 'getPrice doesn\'t equal unit price')
+      return Energy.deployed().then(instance => {
+        return instance.getPrice.call().then(price => {
+          assert.equal(price.valueOf(), 2, 'getPrice doesn\'t equal unit price')
         })
       })
     })
 
-    it('sell() should update current balance', () => {
-      const newBalance = 10
+    it('sell() - update current balance', () => {
+      const amountToSell = 10
+      let balance
       let energy
-      Energy.deployed().then(instance => {
+      return Energy.deployed().then(instance => {
         energy = instance
-        return energy.sell.call(newBalance, {from: seller})
+        return energy.sell(amountToSell, {from: seller})
       }).then(() => {
         return energy.getBalance.call()
       }).then(balance => {
-        assert.equal(balance, newBalance, 'balance not updated')
+        assert.equal(amountToSell, balance.valueOf(), 'balance not updated')
+      })
+
+    })
+
+    it('consume() - let buyer buy energy', () => {
+      const toConsume = 10
+      const newBalance = 0
+      const value = toConsume
+      let energy
+      return Energy.deployed().then(instance => {
+        energy = instance
+        return energy.consume(toConsume, {from: buyer, value: web3.toWei(value, "ether")})
+      }).then((result) => {
+        return energy.getBalance.call()
+      }).then(balance => {
+        assert.equal(balance.valueOf(), newBalance, 'incorrect amount was consumed')
       })
     })
 })
