a9dbf52bf9f53070056566efbaf289e9e70bd612
Daniel Zvara <dan.zvara@gmail.com>
2017-10-28 15:23:12 +0100

    TestEnergy: add test for sell()
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- truffle/test/TestEnergy.js
+++ truffle/test/TestEnergy.js
@@ -4,11 +4,32 @@ contract('TestEnergy', function(accounts) {
     const seller = accounts[0]
     const buyer = 0x42
 
-    it('getBalance should be 0 after contract is created', function() {
+    it('getBalance should be 0 after contract is created', () => {
         Energy.deployed().then(instance => {
-            energy.getBalance.call({from: buyer}).then(function(balance) {
-              assert.equal(energy.getBalance(), 'balance is not 0')
+            instance.getBalance.call().then(balance => {
+              assert.equal(balance, 0, 'balance is not 0')
             })
         })
+    })
+
+    it('getPrice should equal unit price', () => {
+      Energy.deployed().then(instance => {
+        instance.getPrice.call().then(price => {
+          assert.equal(price, 2, 'getPrice doesn\'t equal unit price')
+        })
+      })
+    })
+
+    it('sell() should update current balance', () => {
+      const newBalance = 10
+      let energy
+      Energy.deployed().then(instance => {
+        energy = instance
+        return energy.sell.call(newBalance, {from: seller})
+      }).then(() => {
+        return energy.getBalance.call()
+      }).then(balance => {
+        assert.equal(balance, newBalance, 'balance not updated')
+      })
     })
 })
