b2b3ee1ce38be9d163a3853b1b5f6a70f197aadb
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-01 16:58:27 +0000

    contract: Return deployed contract address in sell

--- truffle/contracts/EnergyMaster.sol
+++ truffle/contracts/EnergyMaster.sol
@@ -15,13 +15,14 @@ contract Master {
     function Master() public {
     }
 
-    function sell(uint unitPrice, uint capacity) public {
+    function sell(uint unitPrice, uint capacity) public returns(address) {
         Energy energy = new Energy(msg.sender, unitPrice, capacity);
         ContractEntity memory entity;
         entity.contract_addr = energy;
         entity.seller = msg.sender;
         lookup[energy] = contracts.length;
         contracts.push(entity);
+        return energy;
     }
 
     function deregister(address contract_addr) public {
