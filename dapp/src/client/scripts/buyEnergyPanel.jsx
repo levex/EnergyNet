1a0ff48330f495ab3e73533d8fc98cec55edca58
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-02 16:42:38 +0000

    Dapp: Fetch contract information from blockchain

--- dapp/src/client/scripts/buyEnergyPanel.jsx
+++ dapp/src/client/scripts/buyEnergyPanel.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {bonds} from 'oo7-parity';
+import {makeMasterContract, makeContract} from "./blockchain";
 
 export class BuyEnergyPanel extends React.Component {
+
+  async getContracts() {
+    let count = await this.master.contractCount();
+    this.setState({count: count});
+    for (let i = 0; i < count; i++) {
+      const contractEntity = await this.master.contracts(i);
+      const deregistered = contractEntity[2];
+      const contract_addr = contractEntity[0];
+      if (!deregistered) {
+        const contract = makeContract(contract_addr);
+        const offeredAmount = await contract.offeredAmount();
+        const unitPrice = await contract.unitPrice();
+        const newEntry = {
+          offeredAmount: offeredAmount,
+          unitPrice: unitPrice
+        };
+        this.setState((prev) => {
+          return {contracts: prev.contracts.concat([newEntry])}
+        })
+      }
+    }
+    console.log(this.state);
+  }
+
+  constructor() {
+    super();
+    this.master = makeMasterContract();
+    this.state = {contracts: []};
+    this.getContracts();
+  }
+
   render() {
     var tableBody = ""
 
