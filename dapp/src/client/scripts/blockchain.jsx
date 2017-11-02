a4ba6f77a2877edfeb1905af738f3a91a4f9c4f9
Norbert Podsadowski <norbo11@googlemail.com>
2017-11-02 15:49:32 +0000

    dapp: Update master ABI

--- dapp/src/client/scripts/blockchain.jsx
+++ dapp/src/client/scripts/blockchain.jsx
@@ -1,6 +1,6 @@
-import { bonds } from 'oo7-parity'
+import {bonds} from 'oo7-parity'
 
-const ENERGY_MASTER_ADDRESS = "0xFD1867fF6E64DB3B38ea51158A4993F303855CD2";
+const ENERGY_MASTER_ADDRESS = "0x3507Ff52cB28F3eCB32A8ee0b0B00618D2E3dD02";
 
 /* TODO: Import ABI somehow */
 const ENERGY_MASTER_ABI = [
@@ -8,46 +8,43 @@ const ENERGY_MASTER_ABI = [
     "constant": true,
     "inputs": [
       {
-        "name": "seller",
-        "type": "address"
-      }, {
-        "name": "index",
+        "name": "",
         "type": "uint256"
       }
     ],
-    "name": "getSellerContractByIndex",
+    "name": "contracts",
     "outputs": [
       {
-        "name": "",
+        "name": "contract_addr",
         "type": "address"
+      }, {
+        "name": "seller",
+        "type": "address"
+      }, {
+        "name": "deregistered",
+        "type": "bool"
       }
     ],
     "payable": false,
+    "stateMutability": "view",
     "type": "function"
   }, {
     "constant": false,
     "inputs": [
       {
-        "name": "energyContract",
-        "type": "address"
-      }, {
-        "name": "seller",
+        "name": "contract_addr",
         "type": "address"
       }
     ],
-    "name": "registerSeller",
+    "name": "deregister",
     "outputs": [],
     "payable": false,
+    "stateMutability": "nonpayable",
     "type": "function"
   }, {
     "constant": true,
-    "inputs": [
-      {
-        "name": "seller",
-        "type": "address"
-      }
-    ],
-    "name": "getSellerContractCount",
+    "inputs": [],
+    "name": "contractCount",
     "outputs": [
       {
         "name": "",
@@ -55,9 +52,36 @@ const ENERGY_MASTER_ABI = [
       }
     ],
     "payable": false,
+    "stateMutability": "view",
     "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "unitPrice",
+        "type": "uint256"
+      }, {
+        "name": "amountOffered",
+        "type": "uint256"
+      }
+    ],
+    "name": "sell",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   }
-];
+]
 
 export function makeMasterContract() {
   return bonds.makeContract(ENERGY_MASTER_ADDRESS, ENERGY_MASTER_ABI);
