bad4fed670a26f3e233dc45e4edda55ac2fe56e4
Vincenzo Pandolfo <pandolfovince@gmail.com>
2017-11-09 15:28:58 +0000

    deploy: starting meter when calling deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,7 @@ set -e
 HOME=$(pwd)
 cd $HOME/truffle && truffle build
 cd $HOME/dapp && npm install && webpack && ./add_to_parity.sh
-cd $HOME/parity && ./run.sh
+cd $HOME/parity && ./run.sh &
+cd $HOME && pip install -r requirements.txt
+python client/meter/api.py &
+./client/meter/mongostarter.sh &
