fdd590b851f17e8488f298156b1198ed9f3fe40e
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-15 15:48:59 +0000

    meter: Implement blockchain query functions for my contracts

---
+++ client/meter/blockchain.js
@@ -0,0 +1,63 @@
+const oo7parity = require('oo7-parity')
+const bonds = oo7parity.bonds
+const ENERGY_MASTER_ABI = require('./abis/abi_master');
+const ENERGY_ABI = require('./abis/abi');
+const ENERGY_MASTER_ADDRESS = "0x3507Ff52cB28F3eCB32A8ee0b0B00618D2E3dD02";
+
+const EnergyMaster = bonds.makeContract(ENERGY_MASTER_ADDRESS, ENERGY_MASTER_ABI);
+
+function makeEnergyContract(address) {
+  return bonds.makeContract(address, ENERGY_ABI);
+}
+
+async function myAccount() {
+  return await bonds.me;
+}
+
+async function myContracts() {
+  const sellerContracts = await mySellerContracts();
+  const buyerContracts = await myBuyerContracts();
+  return {sellerContracts, buyerContracts};
+}
+
+async function mySellerContracts() {
+  const account = await myAccount();
+  const count = await EnergyMaster.contractCount();
+  const contracts = [];
+  for (let i = 0; i < count; i++) {
+    const contractEntity = await EnergyMaster.contracts(i);
+    const contractAddr = contractEntity[0];
+    const contract = makeEnergyContract(contractAddr);
+    const seller = await contract.seller();
+    if (seller === account) {
+      const offeredAmount = await contract.offeredAmount();
+      const unitPrice = await contract.unitPrice();
+      contracts.push({ contractAddr, unitPrice, offeredAmount })
+    }
+  }
+  return contracts;
+}
+
+async function myBuyerContracts() {
+  const account = await myAccount();
+  const count = await EnergyMaster.contractCount();
+  const contracts = [];
+  for (let i = 0; i < count; i++) {
+    const contractEntity = await EnergyMaster.contracts(i);
+    const contractAddr = contractEntity[0];
+    const contract = makeEnergyContract(contractAddr);
+    const remainingAmount = await contract.remainingEnergy(account);
+    if (remainingAmount.greaterThan(0)) {
+      const unitPrice = await contract.unitPrice();
+      contracts.push({ contractAddr, unitPrice, remainingAmount })
+    }
+  }
+  return contracts;
+}
+
+module.exports = {
+  myAccount,
+  myBuyerContracts,
+  mySellerContracts,
+  myContracts
+}
--- client/meter/routes/view.js
+++ client/meter/routes/view.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/all_contracts', (res, req) => {
+const blockchain = require('../blockchain');
 
+router.get('/available_contracts', (req, res) => {
+  blockchain.availableContracts()
+    .then(contracts => res.status(200).json(contracts))
+    .catch(() => res.status(500).end())
 })
 
-router.get('/my_contracts', (res, req) => {
-
+router.get('/my_contracts', (req, res) => {
+  blockchain.myContracts()
+    .then(contracts => res.status(200).json(contracts))
+    .catch(() => res.status(500).end())
 })
 
-router.get('/my_seller_contracts', (res, req) => {
-
+router.get('/my_seller_contracts', (req, res) => {
+  blockchain.mySellerContracts()
+    .then(contracts => res.status(200).json(contracts))
+    .catch(() => res.status(500).end())
 })
 
-router.get('/my_buyer_contracts', (res, req) => {
-
+router.get('/my_buyer_contracts', (req, res) => {
+  blockchain.myBuyerContracts()
+    .then(contracts => res.status(200).json(contracts))
+    .catch(() => res.status(500).end())
 })
 
 module.exports = router
