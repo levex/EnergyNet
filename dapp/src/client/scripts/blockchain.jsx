1ffa7d092a9b34cf35cdfbabc1e20e8c51b52b72
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-15 19:06:29 +0000

    dapp: Change master contract address

--- dapp/src/client/scripts/blockchain.jsx
+++ dapp/src/client/scripts/blockchain.jsx
@@ -2,7 +2,7 @@ import {bonds} from 'oo7-parity'
 import {ENERGY_ABI} from "./abis/abi.js";
 import {ENERGY_MASTER_ABI} from "./abis/abi_master.js";
 
-const ENERGY_MASTER_ADDRESS = "0x3507Ff52cB28F3eCB32A8ee0b0B00618D2E3dD02";
+const ENERGY_MASTER_ADDRESS = "0x520fF2C06fB1ee32eB9e4f1EedecB985869769Ab";
 
 export function makeMasterContract() {
   return bonds.makeContract(ENERGY_MASTER_ADDRESS, ENERGY_MASTER_ABI);
