3fdc3d079baead002a63eb82f85afb90a22a43d7
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-15 15:17:58 +0000

    abis: Produce abis in CommonJS Module Syntax instead of ES6
    https://stackoverflow.com/questions/38296667/getting-unexpected-token-export

--- dapp/src/client/scripts/blockchain.jsx
+++ dapp/src/client/scripts/blockchain.jsx
@@ -1,6 +1,6 @@
 import {bonds} from 'oo7-parity'
-import {ENERGY_ABI} from "./abis/abi.js";
-import {ENERGY_MASTER_ABI} from "./abis/abi_master.js";
+import ENERGY_ABI from "./abis/abi.js";
+import ENERGY_MASTER_ABI from "./abis/abi_master.js";
 
 const ENERGY_MASTER_ADDRESS = "0x520fF2C06fB1ee32eB9e4f1EedecB985869769Ab";
 
