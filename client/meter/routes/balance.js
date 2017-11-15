e73d841bbc11af9214238484f59da413e8e04fca
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-15 14:29:57 +0000

    app: Fix balance api

--- client/meter/routes/balance.js
+++ client/meter/routes/balance.js
@@ -4,7 +4,10 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   const balance = oo7parity.bonds.balance(oo7parity.bonds.me)
-  res.status(200).json({ balance: balance })
+  balance.then((balance) => {
+    const formattedBalance = oo7parity.formatBalance(balance)
+    res.status(200).json({ balance: formattedBalance })
+  })
 })
 
 module.exports = router
