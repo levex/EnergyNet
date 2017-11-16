7c9846310fc2c715e7a1bd33a4d166d6fbd7a659
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-16 23:56:16 +0000

    fixup! deploy: add deploy script to create new user and start simulation

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ check_dep truffle
 check_dep npm
 check_dep parity
 check_dep python3
-check_dep pip3.5
+check_dep pip3
 check_dep mongod
 
 ENERGY_MIN=-100
@@ -23,7 +23,7 @@ HOME=$(pwd)
 cd $HOME/truffle && rm -rf build && truffle compile && truffle build
 cd $HOME/dapp && npm install && webpack && ./add_to_parity.sh
 cd $HOME/parity && ./run.sh &
-cd $HOME && pip3.5 install --user -r requirements.txt
+cd $HOME && pip3 install --user -r requirements.txt
 cd $HOME/client/meter && npm install && npm start &
 cd $HOME/simulation && python3 meter.py -e $ENERGY_MIN $ENERGY_MAX &
 
