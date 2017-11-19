1fcefcd409c96c9173d1322fc4695881c3f82508
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-11-19 00:29:12 +0000

    meter test: Change type from float to int

--- simulation/tests/test_meter.py
+++ simulation/tests/test_meter.py
@@ -10,29 +10,29 @@ class SimulatedMeterClientTest(unittest.TestCase):
 
     @patch('simulation.meter.requests.post')
     def test_sell_energy(self, mock_post):
-        simulation.meter.tick((2.0, 2.0))
+        simulation.meter.tick((2, 2))
 
         mock_post.assert_called_once_with(
             simulation.meter.METER_API_BASE_URL + "transaction/sell",
             data=json.dumps({
-                "amount": 2.0,
+                "amount": 2,
                 "price": 1,
             }),
             headers=simulation.meter.API_CALL_HEADER,
         )
 
-        self.assertEqual(simulation.meter.simulation_metrics["produced"], 2.0)
+        self.assertEqual(simulation.meter.simulation_metrics["produced"], 2)
 
     @patch('simulation.meter.requests.post')
     def test_consume_energy(self, mock_post):
-        simulation.meter.tick((-2.0, -2.0))
+        simulation.meter.tick((-2, -2))
 
         mock_post.assert_called_once_with(
             simulation.meter.METER_API_BASE_URL + "transaction/consume",
             data=json.dumps({
-                "amount": 2.0,
+                "amount": 2,
             }),
             headers=simulation.meter.API_CALL_HEADER,
         )
 
-        self.assertEqual(simulation.meter.simulation_metrics["consumed"], 2.0)
+        self.assertEqual(simulation.meter.simulation_metrics["consumed"], 2)
