76132b54e4b716be554c75635b6ed9385d5443f7
Levente Kurusa <lkurusa@acm.org>
2017-11-28 19:34:49 +0000

    transaction: record stats into influxdb database
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- client/meter/routes/transaction.js
+++ client/meter/routes/transaction.js
@@ -6,6 +6,16 @@ router.post('/buy', (req, res) => {
   const body = req.body;
   const contract = body.contract;
   const amount = body.amount;
+
+  fetch('http://localhost:8086/write?db=energy', {
+    method: 'POST',
+    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	      },
+    body: 'energy_bought amount=' + amount,
+  })
+
   blockchain.buyEnergy(contract, amount)
     .then(() => res.status(200).end())
     .catch(() => res.status(400).end())
@@ -15,6 +25,16 @@ router.post('/sell', (req, res) => {
   const body = req.body;
   const price = body.price;
   const amount = body.amount;
+
+  fetch('http://localhost:8086/write?db=energy', {
+    method: 'POST',
+    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	      },
+    body: 'energy_sold amount=' + amount,
+  })
+
   blockchain.sellEnergy(price, amount)
     .then(() => res.status(200).end())
     .catch(() => res.status(400).end())
@@ -23,6 +43,16 @@ router.post('/sell', (req, res) => {
 router.post('/consume', (req, res) => {
   const body = req.body;
   const amount = body.amount;
+
+  fetch('http://localhost:8086/write?db=energy', {
+    method: 'POST',
+    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	      },
+    body: 'energy_consumed amount=' + amount,
+  })
+
   blockchain.consumeEnergy(amount)
     .then(() => res.status(200).end())
     .catch(reason => res.status(500).send(reason))
