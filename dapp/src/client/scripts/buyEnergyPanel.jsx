dc6f99d1572148e075276d5f13279c1c2d2e1555
Norbert Podsadowski <norbo11@googlemail.com>
2017-12-01 04:16:59 +0000

    dapp: Remove table body calculation from buyEnergyPanel, now happens in seperate component

--- dapp/src/client/scripts/buyEnergyPanel.jsx
+++ dapp/src/client/scripts/buyEnergyPanel.jsx
@@ -4,29 +4,6 @@ import {AvailableContractsTable} from './availableContractsTable'
 
 export class BuyEnergyPanel extends React.Component {
   render() {
-    var tableBody = Object.keys(this.props.contracts).map(address => {
-      const contract = this.props.contracts[address];
-      return (
-        <tr key={contract.address}>
-          <td>Some date</td>
-          <td>{contract.offeredAmount}
-            kWh/day</td>
-          <td>£{contract.unitPrice}/kWh</td>
-          <td>
-            <form role="form">
-              <InputBond placeholder="kWh/day" bond={this.props.amountBond} style={{width: "100%"}} action>
-                <input />
-                {contract.tx === null
-                  ? <BButton className="btn btn-primary" content="Buy Energy" onClick={() => this.props.buyEnergy(address)}/>
-                  : <TransactionProgressLabel value={contract.tx}/>
-                }
-              </InputBond>
-            </form>
-          </td>
-        </tr>
-      );
-    });
-
     return (<div className="panel panel-default">
       <div className="panel-heading">
         <i className="fa fa-bar-chart-o fa-fw"></i>
@@ -36,7 +13,6 @@ export class BuyEnergyPanel extends React.Component {
       <div className="panel-body">
         <AvailableContractsTable contracts={this.props.contracts} amountBond={this.props.amountBond} buyEnergy={this.props.buyEnergy}/>
       </div>
-      {/* /.panel-body */}
     </div>)
   }
 }
