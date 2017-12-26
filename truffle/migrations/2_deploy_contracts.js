a06b22aae637ae86c6c6a8752574ec5a753c7fc9
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-12-26 15:54:40 +0800

    truffle: remove example files

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,13 +1,8 @@
-const ConvertLib = artifacts.require("./ConvertLib.sol");
-const MetaCoin = artifacts.require("./MetaCoin.sol");
 const Master = artifacts.require('./Master.sol');
 
 var accounts;
 web3.eth.getAccounts(function(err,res) { accounts = res; });
 
 module.exports = (deployer) => {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(Master);
 };
