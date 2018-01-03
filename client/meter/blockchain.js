4d214017c2a1b511fddd1ead4a50fc7331c8ea57
Daniel Zvara <dan.zvara@gmail.com>
2018-01-03 18:39:12 +0100

    client: cleanup blockchain utility module
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- client/meter/blockchain.js
+++ client/meter/blockchain.js
@@ -2,8 +2,9 @@ const oo7parity = require("oo7-parity");
 const bonds = oo7parity.bonds;
 const ENERGY_MASTER_ABI = require("./abis/abi_master");
 const ENERGY_ABI = require("./abis/abi");
-const ENERGY_MASTER_ADDRESS = "0x7B7DC4FdB4eAf8168FBC73a9b67f15bB559c87cC";
+const ENERGY_MASTER_ADDRESS = "0x7B019fD19f8b75Ff00089Dc93411dFB04e6D48Ca";
 const bigNumber = require("bignumber.js");
+const recorder = require("./recorder");
 
 const EnergyMaster = bonds.makeContract(ENERGY_MASTER_ADDRESS, ENERGY_MASTER_ABI);
 
@@ -79,15 +80,8 @@ async function init() {
   inited = true;
 }
 
-function send_energy_metric(name, amount) {
-  fetch("http://localhost:8086/write?db=energy", {
-    method: "POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    body: name + " amount=" + amount,
-  });
+function isInited() {
+  return inited;
 }
 
 function makeEnergyContract(address) {
@@ -101,7 +95,8 @@ async function myAccount() {
 async function myContracts() {
   const sellerContracts = await mySellerContracts();
   const buyerContracts = await myBuyerContracts();
-  return {sellerContracts, buyerContracts};
+  return contracts;
+  //return {sellerContracts, buyerContracts};
 }
 
 async function mySellerContracts() {
@@ -118,7 +113,6 @@ async function myBuyerContracts() {
     .map((contractAddr) => contracts[contractAddr]);
 }
 
-
 async function availableContracts() {
   if (!inited) return Promise.reject({ msg: "Blockchain unsynced" });
   return Object.keys(contracts)
@@ -132,7 +126,6 @@ async function buyEnergy(contractAddress, amount) {
     return Promise.reject({ msg: "Wrong contact address or amount", value: amount });
   }
 
-  send_energy_metric("energy_bought", amount);
   const contract = makeEnergyContract(contractAddress);
   logs.push({
     action: "buy",
@@ -148,7 +141,8 @@ async function sellEnergy(price, amount) {
     return Promise.reject({ msg: "Unable to sell energy", value: amount, price: price});
   }
 
-  send_energy_metric("energy_sold", amount);
+  recorder.record_sold_energy(amount);
+  recorder.record_sell_price(price);
 
   logs.push({
     // FIXME: Log contract address
@@ -160,66 +154,6 @@ async function sellEnergy(price, amount) {
   return await EnergyMaster.sell(price, amount);
 }
 
-async function consumeEnergyFromContract(contractAddress, amount) {
-  if (!inited) return Promise.reject({ msg: "Blockchain unsynced" });
-  const contract = makeEnergyContract(contractAddress);
-  const price = await contract.unitPrice();
-  const cost = price.mul(amount);
-  send_energy_metric("energy_consumed", amount);
-  logs.push({
-    action: "consume",
-    amount: amount,
-    contract: contractAddress
-  });
-  return await contract.consume(amount, {value: cost});
-}
-
-async function consumeEnergy(amount) {
-  if (!inited) return Promise.reject({ msg: "Blockchain unsynced" });
-  if (amount < 0) {
-    return Promise.reject({ msg: "negative amount", value: amount });
-  }
-
-  let energyBalance = await myEnergyBalance();
-  if (energyBalance < amount) {
-    try {
-      await autoBuy(amount - energyBalance);
-
-      let count = 0;
-      while (energyBalance < amount) {
-        energyBalance = await myEnergyBalance();
-        count++;
-        if (count == 30) {
-          return Promise.reject({ msg: "failed to autoBuy", value: amount });
-        }
-      }
-    } catch(e) {
-      return Promise.reject(e);
-    }
-  }
-
-  const contracts = await myBuyerContracts();
-  let toConsume = amount;
-  // cheapest first
-  contracts.sort((a, b) => {
-    return a.unitPrice - b.unitPrice;
-  });
-
-  let txs = [];
-  for (const contract of contracts) {
-    if (toConsume === 0) break;
-    const deduction = Math.min(toConsume, contract.remainingAmount);
-    txs.push({ address: contract.address, amount: deduction});
-    toConsume -= deduction;
-  }
-
-  if (toConsume > 0) {
-    return Promise.reject({ msg: "Unable to consume energy", value: toConsume });
-  }
-
-  const promises = txs.map((tx) => consumeEnergyFromContract(tx.address, tx.amount));
-  return Promise.all(promises);
-}
 
 async function myEnergyBalance() {
   if (!inited) return Promise.reject({ msg: "Blockchain unsynced" });
@@ -229,34 +163,6 @@ async function myEnergyBalance() {
     balance = balance.add(contract.remainingAmount);
   }
   return balance;
-}
-
-async function autoBuy(amount) {
-  if (!inited) return Promise.reject({ msg: "Blockchain unsynced" });
-  if (!amount || amount < 0) {
-    return Promise.reject({ msg: "Invalid amount", value: amount });
-  }
-
-  const contracts = await availableContracts();
-  let toBuy = amount;
-  // cheapest first
-  contracts.sort((a, b) => {
-    return a.unitPrice - b.unitPrice;
-  });
-  let txs = [];
-  for (const contract of contracts) {
-    if (toBuy === 0) break;
-    const deduction = Math.min(toBuy, contract.offeredAmount);
-    txs.push({ address: contract.address, amount: deduction});
-    toBuy -= deduction;
-  }
-
-  if (toBuy > 0) {
-    return Promise.reject({ msg: "Insufficient energy over network", value: toBuy });
-  }
-
-  const promises = txs.map((tx) => buyEnergy(tx.address, tx.amount));
-  return Promise.all(promises);
 }
 
 async function updateBlock(blockNumber) {
@@ -287,12 +193,16 @@ async function updateBlockchain(blockNumber) {
 
 init();
 
+function log(msg) {
+  logs.push(msg);
+}
+
 function getLog() {
   return logs.slice(-10);
 }
 
 module.exports = {
-  send_energy_metric,
+  makeEnergyContract,
   myAccount,
   myBuyerContracts,
   mySellerContracts,
@@ -301,8 +211,8 @@ module.exports = {
   availableContracts,
   buyEnergy,
   sellEnergy,
-  consumeEnergy,
-  autoBuy,
   updateBlockchain,
-  getLog
+  getLog,
+  log,
+  isInited,
 };
