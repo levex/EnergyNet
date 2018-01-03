25d5e895b03e74decc5491017c71dbd2b10b1d12
Daniel Zvara <dan.zvara@gmail.com>
2018-01-03 18:39:46 +0100

    simulation, meter: re-write simulated meter.
    Use continuously running loop to generate tick events.
    Accept remote configuration to configure these events.
    
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- simulation/meter.py
+++ simulation/meter.py
@@ -2,10 +2,10 @@ import argparse
 import json
 import random
 import requests
-
+import time
+import threading
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
 
 METER_API_BASE_URL = "http://localhost:3000/"
 API_CALL_HEADER = {
@@ -17,6 +17,13 @@ args = None
 simulation_metrics = {
     "consumed": 0,
     "sold": 0,
+}
+
+simulation_config = {
+    "price": 1,
+    "energy_input": 0,
+    "input_noise": 0,
+    "enabled": False,
 }
 
 
@@ -54,21 +61,10 @@ def sell_energy(amount, price):
         METER_API_BASE_URL + "transaction/sell",
         data=json.dumps({
             "amount": amount,
-            "price": price,
         }),
         headers=API_CALL_HEADER,
     )
     simulation_metrics["sold"] += amount
-
-
-def tick(excess_interval):
-    produced_energy = random.randint(*excess_interval)
-
-    if produced_energy < 0:
-        consume_energy(-produced_energy)
-    elif produced_energy > 0:
-        sell_energy(produced_energy, args.price)
-
 
 class MeterHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -80,7 +76,6 @@ class MeterHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             return
         elif self.path == "/metrics":
-
             self.send_response(200)
             self.end_headers()
             self.wfile.write(bytes(
@@ -88,14 +83,72 @@ class MeterHTTPRequestHandler(BaseHTTPRequestHandler):
                 "utf-8",
             ))
             return
+        elif self.path == "/config":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(bytes(
+                json.dumps(simulation_config),
+                "utf-8",
+            ))
+            return
 
         self.send_response(404)
         self.end_headers()
 
+    def do_POST(self):
+      global simulation_config
+      if self.path == '/config':
+          content_length = int(self.headers['Content-Length'])
+          post_data = str(self.rfile.read(content_length), "utf-8")
+          self.send_response(200)
+          self.end_headers()
+
+          print(post_data)
+          config = json.loads(post_data)
+          print("Setting config to: " + str(config))
+          if (config["price"] !== simulation_config["price"]):
+              print("Setting price to: " + str(config["price"]))
+              requests.post(
+                  METER_API_BASE_URL + "config/price",
+                  data=json.dumps({
+                      "price": config["price"],
+                  }),
+                  headers=API_CALL_HEADER,
+              )
+
+          simulation_config = config
+
+          return
+
+      self.send_response(404)
+      self.end_headers()
+
+def tick():
+    input_noise = simulation_config["input_noise"]
+    energy_input = simulation_config["energy_input"] + \
+                    random.randint(-input_noise, input_noise)
+
+    print("Energy outcome: " + str(energy_input) + "kWh")
+    if energy_input > 0:
+      sell_energy(energy_input, simulation_config["price"])
+    elif energy_input < 0:
+      consume_energy(-energy_input)
 
 if __name__ == "__main__":
     args = parse_arguments()
+    simulation_config["price"] = args.price
 
     server_address = ("", args.port)
     httpd = HTTPServer(server_address, MeterHTTPRequestHandler)
-    httpd.serve_forever()
+    server_thread = threading.Thread(target = httpd.serve_forever)
+    server_thread.daemon = True
+
+    try:
+      server_thread.start()
+    except:
+      httpd.shutdown()
+
+    while True:
+      if (simulation_config["enabled"]):
+          tick()
+      time.sleep(1)
