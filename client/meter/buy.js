11d7291dcd6052a6c9736dad75d292c8a0ab183f
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-01-11 18:37:56 +0000

    client: bulk buy

--- client/meter/buy.js
+++ client/meter/buy.js
@@ -2,15 +2,17 @@ const blockchain = require("./blockchain");
 const recorder = require("./recorder");
 const BigNumber = require("bignumber.js");
 
-async function autoBuy(amount) {
-  let amountBigNumber = new BigNumber(amount);
-  if (!blockchain.isInited()) return Promise.reject({ msg: "Blockchain unsynced" });
-  if (!amount || amount < 0) {
-    return Promise.reject({ msg: "Invalid amount", value: amount });
-  }
+const BUY_INTERVAL = 10000;
 
+let autoBuyAmount = new BigNumber(0);
+
+async function actuallyAutoBuy() {
+  if (autoBuyAmount.lte(0)) {
+    autoBuyAmount = new BigNumber(0);
+    return;
+  }
   const contracts = await blockchain.availableContracts();
-  let toBuy = amountBigNumber;
+  let toBuy = autoBuyAmount;
   // cheapest first
   contracts.sort((a, b) => {
     return a.unitPrice - b.unitPrice;
@@ -28,9 +30,20 @@ async function autoBuy(amount) {
     return Promise.reject({ msg: "Insufficient energy over network", value: toBuy });
   }
 
-  const promises = txs.map((tx) => blockchain.buyEnergy(tx.address, tx.amount));
+  const promises = txs.map((tx) => blockchain.buyEnergy(tx.address, tx.amount).then(() => autoBuyAmount = autoBuyAmount.sub(tx.amount)));
   return Promise.all(promises);
 }
+
+async function autoBuy(amount) {
+  if (!blockchain.isInited()) return Promise.reject({ msg: "Blockchain unsynced" });
+  if (!amount || amount < 0) {
+    return Promise.reject({ msg: "Invalid amount", value: amount });
+  }
+  let amountBigNumber = new BigNumber(amount);
+  autoBuyAmount = autoBuyAmount.add(amountBigNumber);
+}
+
+setInterval(actuallyAutoBuy, BUY_INTERVAL);
 
 module.exports = {
   autoBuy,
